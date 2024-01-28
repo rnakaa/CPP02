78be9a9bc59443a9ad6733ad7a421df361c62c47
42rnaka <rnaka@student.42tokyo.jp>
2024-01-28 18:55:56 +0900

    fa

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,33 @@
+#include"Fixed.hpp"
+
+Fixed::Fixed(int integer){
+	std::cout << "Default constructor called\n";
+	this->integer = integer;
+}
+
+Fixed::~Fixed(){
+	std::cout << "Destructor called\n";
+}
+
+Fixed::Fixed(Fixed &cp){ 
+	std::cout << "Copy constructor called\n";
+	*this = cp;
+}
+
+Fixed &Fixed::operator=(Fixed &cp){
+	std::cout << "Copy assignment operator called\n";
+	if(this == &cp)
+		return *this;
+	integer = cp.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits(void){
+	std::cout << "getRawBits member function called\n";
+	return (integer);
+}
+
+void Fixed::setRawBits(int setInt){
+	std::cout << "setRawBits member function called\n";
+	integer = setInt;
+}
